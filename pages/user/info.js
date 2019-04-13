6e4ac8638ba121b257aa6b722c90de93ac6c4f23
mxj <332820892@qq.com>
2019-04-13 23:38:46 +0800

    图标修改

--- pages/user/info.js
+++ pages/user/info.js
@@ -64,20 +64,20 @@ Page({
       }
     ]
   },
-  kindToggle: function(e) {
-    var id = e.currentTarget.id,
-      list = this.data.list;
-    for (var i = 0, len = list.length; i < len; ++i) {
-      if (list[i].id == id) {
-        list[i].open = !list[i].open
-      } else {
-        list[i].open = false
-      }
-    }
-    this.setData({
-      list: list
-    });
-  },
+  // kindToggle: function(e) {
+  //   var id = e.currentTarget.id,
+  //     list = this.data.list;
+  //   for (var i = 0, len = list.length; i < len; ++i) {
+  //     if (list[i].id == id) {
+  //       list[i].open = !list[i].open
+  //     } else {
+  //       list[i].open = false
+  //     }
+  //   }
+  //   this.setData({
+  //     list: list
+  //   });
+  // },
 
   /**
    * 生命周期函数--监听页面加载
